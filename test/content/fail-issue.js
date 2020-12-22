1fadc1a327fc8c06e3f16d97327810748f578681
Marcel Raschke <42359664+MarcelRaschke@users.noreply.github.com>
2020-12-22 08:36:42 +0100

    Initialize greenkeeper/semver-pin-6.0.0

---
+++ test/content/fail-issue.js
@@ -0,0 +1,74 @@
+describe('Fails issue content', async () => {
+  test('Displays statuses and checks correctly', async () => {
+    const content = require('../../content/fail-issue')
+
+    const issueContent = content({
+      version: '14.2.0',
+      dependencyLink: 'http://lol.cat',
+      owner: 'gilliam',
+      repo: 'brazil',
+      base: 'master',
+      head: 'feat/wings',
+      dependency: 'tuttle',
+      oldVersionResolved: '14.0.0',
+      dependencyType: 'dependencies',
+      statuses: [
+        {
+          state: 'success',
+          context: 'continuous-integration/travis-ci/push',
+          description: 'The Travis CI build passed',
+          target_url: 'http://lol.cat'
+        }, {
+          state: 'failure',
+          context: 'continuous-integration/travis-ci/pr',
+          description: 'The Travis CI build failed',
+          target_url: 'http://lol.cat'
+        }, {
+          state: 'success',
+          context: 'continuous-integration/travis-ci/pr',
+          description: '<a href="https://travis-ci.com/club/mate/builds/77480025"><img src="https://travis-ci.com/images/stroke-icons/icon-passed.png" height="11"> The build</a> **passed**.'
+        }
+      ],
+      release: '14.2.0',
+      diffCommits: `<details>
+      <summary>Commits</summary>
+      A list of commits
+    </details>`,
+      monorepoGroupName: ''
+    })
+    expect(issueContent).toBeTruthy()
+    expect(issueContent).toMatchSnapshot()
+  })
+
+  test('Displays monorepo issue correctly', async () => {
+    const content = require('../../content/fail-issue')
+
+    const issueContent = content({
+      version: '14.2.0',
+      dependencyLink: 'http://lol.cat',
+      owner: 'gilliam',
+      repo: 'brazil',
+      base: 'master',
+      head: 'feat/wings',
+      dependency: 'tuttle',
+      oldVersionResolved: '14.0.0',
+      dependencyType: 'dependencies',
+      statuses: [
+        {
+          state: 'success',
+          context: 'continuous-integration/travis-ci/pr',
+          description: '<a href="https://travis-ci.com/club/mate/builds/77480025"><img src="https://travis-ci.com/images/stroke-icons/icon-passed.png" height="11"> The build</a> **passed**.'
+        }
+      ],
+      release: '14.2.0',
+      diffCommits: `<details>
+      <summary>Commits</summary>
+      A list of commits
+    </details>`,
+      monorepoGroupName: 'babel7',
+      packageUpdateList: ''
+    })
+    expect(issueContent).toBeTruthy()
+    expect(issueContent).toMatchSnapshot()
+  })
+})
