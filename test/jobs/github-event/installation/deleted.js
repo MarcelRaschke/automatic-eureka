4ea1bf0dc3e1c79154f0449b0a8f7c4e29782a87
Marcel Raschke <42359664+MarcelRaschke@users.noreply.github.com>
2020-12-22 08:36:48 +0100

    Initialize greenkeeper/@octokit/rest-16.27.0

---
+++ test/jobs/github-event/installation/deleted.js
@@ -0,0 +1,40 @@
+const dbs = require('../../../../lib/dbs')
+const githubEvent = require('../../../../jobs/github-event')
+
+test('github-event installation deleted', async () => {
+  expect.assertions(3)
+  const { installations, repositories } = await dbs()
+
+  await Promise.all([
+    installations.put({
+      _id: '2',
+      installation: 1
+    }),
+    repositories.put({
+      _id: '4',
+      accountId: '2'
+    })
+  ])
+
+  const newJobs = await githubEvent({
+    type: 'installation',
+    action: 'deleted',
+    installation: { account: { id: 2 } }
+  })
+
+  expect(newJobs).toBeFalsy()
+
+  try {
+    await installations.get('2')
+  } catch (e) {
+    // installation is deleted
+    expect(e.status).toBe(404)
+  }
+
+  const repos = await repositories.query('by_account', {
+    key: '2'
+  })
+
+  // repositories are deleted
+  expect(repos.rows).toHaveLength(0)
+})
