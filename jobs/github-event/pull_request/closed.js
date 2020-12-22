8799247ca8b342af90389e46840d4736bb286bad
Marcel Raschke <42359664+MarcelRaschke@users.noreply.github.com>
2020-12-22 08:44:12 +0100

    Initialize greenkeeper/@octokit/rest-16.43.1

---
+++ jobs/github-event/pull_request/closed.js
@@ -0,0 +1,46 @@
+const _ = require('lodash')
+
+const githubQueue = require('../../../lib/github-queue')
+const dbs = require('../../../lib/dbs')
+const upsert = require('../../../lib/upsert')
+const env = require('../../../lib/env')
+const { maybeUpdatePaymentsJob } = require('../../../lib/payments')
+
+module.exports = async function (data) {
+  const { repositories } = await dbs()
+  const { pull_request: pullRequest, repository, installation } = data
+  const prDocId = `${repository.id}:pr:${pullRequest.id}`
+
+  try {
+    await repositories.get(prDocId)
+  } catch (err) {
+    if (err.status === 404) return
+    throw err
+  }
+
+  const prdoc = await upsert(
+    repositories,
+    prDocId,
+    _.pick(pullRequest, ['state', 'merged'])
+  )
+  if (!prdoc.merged || !prdoc.initial) return
+
+  let repoDoc = await repositories.get(String(repository.id))
+
+  const [owner, repo] = repository.full_name.split('/')
+
+  repoDoc = await upsert(repositories, String(repository.id), {
+    enabled: true
+  })
+  try {
+    await githubQueue(installation.id).write(github => github.gitdata.deleteRef({
+      owner,
+      repo,
+      ref: `heads/${prdoc.head}`
+    }))
+  } catch (e) {}
+
+  if (!env.IS_ENTERPRISE) {
+    return maybeUpdatePaymentsJob({ accountId: repoDoc.accountId, isPrivate: repoDoc.private })
+  }
+}
