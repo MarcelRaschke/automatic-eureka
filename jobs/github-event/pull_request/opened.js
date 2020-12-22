7775a0451a9da0062a929eae1fef6e8f1ad08a77
Marcel Raschke <42359664+MarcelRaschke@users.noreply.github.com>
2020-12-22 08:37:59 +0100

    Initialize greenkeeper/rollbar-2.8.1

---
+++ jobs/github-event/pull_request/opened.js
@@ -0,0 +1,63 @@
+const dbs = require('../../../lib/dbs')
+const env = require('../../../lib/env')
+const getConfig = require('../../../lib/get-config')
+const { getActiveBilling, getAccountNeedsMarketplaceUpgrade } = require('../../../lib/payments')
+const githubQueue = require('../../../lib/github-queue')
+
+module.exports = async function (data) {
+  const { repositories } = await dbs()
+  const { pull_request: pullRequest, repository, installation } = data
+  const repositoryId = String(repository.id)
+  const prDocId = `${repositoryId}:pr:${pullRequest.id}`
+  const repoDoc = await repositories.get(repositoryId)
+  const [owner, repo] = repository.full_name.split('/')
+  const config = getConfig(repoDoc)
+
+  const wasCreatedByGreenkeeper = pullRequest.user.type === 'Bot' && pullRequest.user.login.substr(0, 11) === 'greenkeeper'
+  if (wasCreatedByGreenkeeper) return
+
+  const isInitialGreenkeeperBranch = pullRequest.head.ref === `${config.branchPrefix}initial`
+  if (!isInitialGreenkeeperBranch) return
+
+  const accountId = repository.owner.id
+
+  await repositories.put(
+    {
+      _id: prDocId,
+      repositoryId,
+      accountId: accountId,
+      type: 'pr',
+      initial: true,
+      number: pullRequest.number,
+      head: pullRequest.head.ref,
+      state: pullRequest.state,
+      merged: pullRequest.merged,
+      createdAt: new Date().toJSON(),
+      createdByUser: true
+    }
+  )
+
+  if (!repoDoc.private || env.IS_ENTERPRISE) {
+    return
+  }
+
+  const ghqueue = githubQueue(installation.id)
+
+  const billingAccount = await getActiveBilling(accountId)
+  const hasBillingAccount = !!billingAccount
+  const accountNeedsMarketplaceUpgrade = await getAccountNeedsMarketplaceUpgrade(accountId)
+
+  if (!hasBillingAccount || accountNeedsMarketplaceUpgrade) {
+    const targetUrl = accountNeedsMarketplaceUpgrade ? 'https://github.com/marketplace/greenkeeper/' : 'https://account.greenkeeper.io/'
+
+    await ghqueue.write(github => github.repos.createStatus({
+      owner,
+      repo,
+      sha: pullRequest.head.sha,
+      state: 'pending',
+      context: 'greenkeeper/payment',
+      description: 'Payment required, merging will have no effect',
+      target_url: targetUrl
+    }))
+  }
+}
